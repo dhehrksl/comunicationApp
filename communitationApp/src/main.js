85cd122ea4762de6cff1621d84fa6d080cecab81
DESKTOP-4879E3T\TI08 <hyunjunti@gmail.com>
2024-08-14 18:06:26 +0900

    mainView

---
+++ communitationApp/src/main.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>onthebar</Text>
+      <Text>11</Text>
+      <Text>2</Text>
+      <Text>3</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

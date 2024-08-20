ca19979849067904aad7ff9879b52989daebf464
DESKTOP-4879E3T\TI08 <hyunjunti@gmail.com>
2024-08-20 17:57:05 +0900

    navigation add

--- communitationApp/App.js
+++ communitationApp/App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../communitationApp/src/main';
 import JoinMember from '../communitationApp/src/joinMember';
-
+import HomePage from './src/homePage';
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -12,6 +12,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Join" component={JoinMember} options={{ headerShown: false }} />
+        <Stack.Screen name="HomePage" component={HomePage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
